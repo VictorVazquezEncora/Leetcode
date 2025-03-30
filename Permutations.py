146429d8b5748dc13fd8dc5b178dd7647d92e6d1
VictorVazquezEncora <victor.vazquez@encora.com>
2025-03-30 12:12:31 -0700

    Solution added

--- Permutations.py
+++ Permutations.py
@@ -1,1 +1,22 @@
-# Just for the first commit
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        cur_per = []
+        res = []
+
+        def backtrack():
+            if len(cur_per) == len(nums):
+                res.append(cur_per[:])
+                return
+
+            for num in nums:
+                if num not in cur_per:
+                    cur_per.append(num)
+                    backtrack()
+                    cur_per.pop()
+
+        backtrack()
+        return res
